a8c4873e5cd27f1ea4ef0f4254f3f5c463467d9a
Matheus C O Santos <oliveiramatheus77@gmail.com>
2020-06-30 16:39:08 -0300

    Added endpoint for creating and authenticating users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -20,7 +20,7 @@ class AdminSiteTests(TestCase):
         )
 
     def test_user_listed(self):
-        """Test that users are listed on user page"""
+        """Test that user are listed on user page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
